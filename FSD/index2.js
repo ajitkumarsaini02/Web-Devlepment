5f509aad9f8c561885f4082d624242e1b6f7d4cd
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-09 16:14:43 +0530

    ..

---
+++ FSD/index2.js
@@ -0,0 +1,15 @@
+let a= 10;
+let b= '20';
+if(a==b){
+    console.log('a and b are equal');
+}
+else{
+    console.log('a and b are not equal');
+}
+
+if(a===b){
+    console.log('a and b are equal');
+}
+else{
+    console.log('a and b are not equal');
+}
