89d55b09c9ae424ee8e041fedb6ca19110be7d94
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-04-15 21:47:54 +0530

    ..

---
+++ Practice/JS/2_String/prac2.js
@@ -0,0 +1,11 @@
+let msg = "help!";
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg); // "HELP!"
+
+let name = "ApnaCollege";
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna", "Our"));
+
+let str = name.slice(4);
+console.log(str.replace("l", "t").replace("l", "t"));
